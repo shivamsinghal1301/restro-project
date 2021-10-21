fe203780410d0a4a3331d61a746a6d0382130c61
shivamsinghal1301 <shivamsinghal13012002@gmail.com>
2021-10-21 16:08:40 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import app from "./server.js";
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js"
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize:50,
+        wtimeoutMS:2500,
+        useNewUrlParser:true
+    }
+)
+.catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    await RestaurantsDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client)
+    app.listen(port, () => {
+        console.log('listening on port '+port)
+    })
+})
